88ef221871afc779070a1315cf9dd5a1cdebd081
Xaratustrah <shahab@zamail.de>
2016-01-01 21:03:33 +0100

    version correction

--- setup.py
+++ setup.py
@@ -3,6 +3,17 @@
 from otterlog.version import __version__
 from distutils.core import setup
 
+classifiers = [
+    'Environment :: Console',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Intended Audience :: Information Technology',
+    'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Topic :: Communications :: Ham Radio'
+]
+
 setup(
     name='otterlog',
     packages=['otterlog'],  # this must be the same as the name above
@@ -12,6 +23,8 @@ setup(
     author_email='shahab.sanjari@gmail.com',
     url='https://github.com/xaratustrah/otterlog',  # use the URL to the github repo
     download_url='https://github.com/xaratustrah/otterlog/tarball/{}'.format(__version__),
+    scripts=['otterlog'],
+    license='GPL v2',
     keywords=['HAM', 'Logging', 'Amateur Radio', 'QSO'],  # arbitrary keywords
-    classifiers=[],
+    classifiers=classifiers,
 )
