2e3372766d01b45de3b612de50a993e72713b912
xaratustrah <xaratustrah@users.noreply.github.com>
2023-02-06 23:42:33 +0100

    updated pypi packages

--- dolphinlog/version.py
+++ dolphinlog/version.py
@@ -1,2 +1,2 @@
-__version_info__ = (0, 2, 6)
+__version_info__ = (0, 2, 9)
 __version__ = '.'.join('%d' % d for d in __version_info__)
--- setup.py
+++ setup.py
@@ -1,41 +1,4 @@
-# PyPI setup file
+from setuptools import setup
 
-from setuptools import setup, find_packages
-from dolphinlog.version import __version__
-
-# try:
-#     import pypandoc
-#
-#     long_description = pypandoc.convert('README.md', 'rst')
-# except(IOError, ImportError):
-#     long_description = open('README.md').read()
-
-long_description = ''
-
-classifiers = [
-    'Environment :: Console',
-    'Programming Language :: Python :: 3 :: Only',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Natural Language :: English',
-    'Operating System :: OS Independent',
-    'Topic :: Communications :: Ham Radio'
-]
-
-setup(
-    name='dolphinlog',
-    packages=find_packages(),
-    version=__version__,
-    description='Yet another HAM radio logger using SQLite with ADIF-3 export support.',
-    long_description=long_description,
-    author='xaratustrah',
-    url='https://github.com/xaratustrah/dolphinlog',  # use the URL to the github repo
-    download_url='https://github.com/xaratustrah/dolphinlog/tarball/{}'.format(__version__),
-    entry_points={
-        'console_scripts': [
-            'dolphinlog = dolphinlog.__main__:main'
-        ]
-    },
-    license='GPLv3',
-    keywords=['HAM', 'Logging', 'Amateur Radio', 'QSO'],  # arbitrary keywords
-    classifiers=classifiers
-)
+if __name__ == '__main__':
+    setup()
